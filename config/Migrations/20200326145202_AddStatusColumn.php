0067c2eba803092ed787d4524b033d9333a6d1b4
Michiel Keijts <michiel@normit.nl>
2020-03-26 14:57:09 +0100

    Added migration for status

---
+++ config/Migrations/20200326145202_AddStatusColumn.php
@@ -0,0 +1,46 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddStatusColumn extends AbstractMigration
+{
+
+    public function up()
+    {
+        $this->table('cake_api_connector_dataobjects')
+            ->addColumn('status', 'string', [
+                'after' => 'runner',
+                'default' => 'READY',
+                'length' => 32,
+                'null' => false,
+            ])
+            ->addIndex(
+                [
+                    'deleted',
+                    'site_id',
+                    'language',
+                    'foreign_id',
+                    'parent_id',
+                    'parent_model',
+                    'entity',
+                    'status'
+                ],
+                [
+                    'name' => 'status_update',
+                ]
+            )
+            ->update();
+    }
+
+    public function down()
+    {
+
+        $this->table('cake_api_connector_dataobjects')
+            ->removeIndexByName('status_update')
+            ->update();
+
+        $this->table('cake_api_connector_dataobjects')
+            ->removeColumn('status')
+            ->update();
+    }
+}
+
