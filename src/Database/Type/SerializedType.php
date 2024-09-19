11bb687e7f1499b4bafd7083f7510dd8cfbe8cec
Michiel Keijts <michiel@normit.nl>
2024-09-19 16:37:16 +0200

    Update type compatibility

--- src/Database/Type/SerializedType.php
+++ src/Database/Type/SerializedType.php
@@ -1,11 +1,14 @@
 <?php
+declare(strict_types=1);
+
 /*
- * @copyright (C) 2020 Michiel Keijts, Normit
- * 
+ * @copyright (C) 2024 Michiel Keijts, Normit
+ *
  */
 
 namespace CakeApiConnector\Database\Type;
 
+use Cake\Database\Driver;
 use Cake\Database\DriverInterface;
 use Cake\Database\Type\BaseType;
 use PDO;
@@ -19,7 +22,7 @@ use Cake\Database\TypeInterface;
 
 class SerializedType extends BaseType implements TypeInterface
 {
-    public function toPHP($value, DriverInterface $driver)
+    public function toPHP(mixed $value, Driver $driver): mixed
     {
         if ($value === null) {
             return null;
@@ -27,7 +30,7 @@ class SerializedType extends BaseType implements TypeInterface
         return unserialize($value);
     }
 
-    public function marshal($value)
+    public function marshal(mixed $value): mixed
     {
         if (is_array($value) || is_object($value) || $value === null) {
             return $value;
@@ -35,12 +38,12 @@ class SerializedType extends BaseType implements TypeInterface
         return unserialize($value);
     }
 
-    public function toDatabase($value, DriverInterface $driver)
+    public function toDatabase(mixed $value, Driver $driver): mixed
     {
         return serialize($value);
     }
 
-    public function toStatement($value, DriverInterface $driver)
+    public function toStatement(mixed $value, Driver $driver): int
     {
         if ($value === null) {
             return PDO::PARAM_NULL;
